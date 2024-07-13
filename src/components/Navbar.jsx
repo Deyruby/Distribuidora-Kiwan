f3f8bd8245a35713018f115ca90361cf12af4370
Deyruby <deirubyrincon@gmail.com>
2024-07-12 23:03:28 -0400

    avances

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,20 @@ import React from "react";
 
 const Navbar= ()=>{
 
-
+    const whatsappUrl = `https://wa.me/${'56959454869'}?text=${encodeURIComponent('Hola, me gustaría más información.')}`;
+    const facebookUrl= `https://www.facebook.com/profile.php?id=100093007008249&mibextid=LQQJ4d`
+    const instagramUrl = `https://www.instagram.com/distribuidorakiwan?igsh=ZTg3bnZnajhyZ3B3&utm_source=qr`
+   const tiktokUrl = `https://www.tiktok.com/@distribuidorakiwan?_t=8nycqVW2kc9&_r=1`
+    
+    const handleClick=(url)=>{
+        window.open(url, '_blank')
+      }
 
     return(
     <>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Ubicación</a>
+    <a className=" ms-3 navbar-brand" href="#">Ubicación</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -36,10 +43,28 @@ const Navbar= ()=>{
           </ul>
         </li>
       </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search"/>
+      <form className="d-flex ms-3">
+        <input className="form-control me-2" type="search" style={{width: "250px"}} placeholder="Buscar" aria-label="Search"/>
         <button className="btn btn-outline-success" type="submit">Buscar</button>
       </form>
+    </div>
+    <div className="icons">
+    <div className="icon">
+    <i className="fa-brands fa-square-whatsapp" onClick={()=>handleClick(whatsappUrl)} style={{color: "#28e302", fontSize:"35px"}}></i>
+    <span className="tooltiptext">Escribir al Whatsapp</span>
+    </div>
+    <div className="icon">
+    <i className="fa-brands fa-square-facebook ms-3" onClick={()=>handleClick(facebookUrl)} style={{color: "#141cff", fontSize:"35px"}}></i>
+    <span className="tooltiptext">Seguir en Facebook!</span>
+    </div>
+    <div className="icon">
+    <i className="fa-brands fa-instagram ms-3" onClick={()=>handleClick(instagramUrl)} style={{color: "#b61cca", fontSize:"35px"}}></i>
+    <span className="tooltiptext">Seguir en Instagram!</span>
+    </div>
+    <div className="icon">
+    <i className="fa-brands fa-tiktok ms-3 pt-1" onClick={()=>handleClick(tiktokUrl)} style={{color: "black" ,fontSize:"29px"}}></i>
+    <span className="tooltiptext">Seguir en TikTok!</span>
+    </div>
     </div>
   </div>
 </nav>
